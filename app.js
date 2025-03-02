a0282990adaeb833d09fc402a6c9f6269099140b
Martin-Rojas <rojas.martin25@yahoo.com>
2025-03-01 17:25:36 -0800

    update code to display the game board

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alert(`hello`);
+function MakePlayer(name, symbol) {
+  return { name, symbol };
+}
+
+// gameboard
+function GameBoard() {
+  const board = ["", "", "", "", "", "", "", "", ""];
+
+  const resetBoard = ["", "", "", "", "", "", "", "", ""];
+
+  const updateBoard = (index, symbol) => {
+    if(board[index] === ""){
+        board[index] = symbol;
+        return true;
+    }else{
+        return false;
+    }
+  }
+
+  
+}
